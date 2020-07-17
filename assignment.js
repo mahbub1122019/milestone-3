e6d9f315f20de33826535bf53e5281886fb2a99c
mahbub1122019 <mahbub1122019@gmail.com>
2020-07-17 22:32:53 +0600

    finally solved

--- assignment.js
+++ assignment.js
@@ -15,7 +15,7 @@ function feetToMile (feet){
  }
 
  //woodCalculator
- 
+
  function woodCalculator(chair, table, bed){
      var chairCount =chair * 1;
      var tableCount =table * 3;
@@ -23,6 +23,22 @@ function feetToMile (feet){
      var totalCount = chairCount +tableCount +bedCount;
      return totalCount;
  }
+  
+ // brickCalculator
+ function brickCalculator (floor){
+     if (floor <= 10){
+         var feet1 = floor *15;
+         var brick = feet1 *1000;
+     }
+     else if (floor <=20){
+         var feet2 = floor *12;
+         var brick =feet2 *1000;
+     }
+     else if(floor > 20){
+         var feet3 = floor *10;
+         var brick =feet3 *1000;
+     }
+     return brick;
+ }
 
 
-    
