23d8a4946e28ced4d640e71afeb5b40153bf9cab
mahbub1122019 <mahbub1122019@gmail.com>
2020-07-17 21:37:15 +0600

    three already have done

--- assignment.js
+++ assignment.js
@@ -8,6 +8,21 @@ function feetToMile (feet){
  // tinyFriend||||
  
 
+ const tinyFriend = names => {
+ const minimumLength = Math.min(...names.map(name => name.length));
+ const sName = names.find(name => name.length == minimumLength);
+ return sName;
+ }
+
+ //woodCalculator
+ 
+ function woodCalculator(chair, table, bed){
+     var chairCount =chair * 1;
+     var tableCount =table * 3;
+     var bedCount = bed * 5;
+     var totalCount = chairCount +tableCount +bedCount;
+     return totalCount;
+ }
 
 
     
